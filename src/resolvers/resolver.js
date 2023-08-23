2a49f7b6bc21851865857d1054045be7455cc662
Adrien GOGOIS <ad.gogois@gmail.com>
2023-08-23 16:37:30 +0200

    Add getTodo by id

--- src/resolvers/resolver.js
+++ src/resolvers/resolver.js
@@ -2,7 +2,10 @@ import todos from '../database/todos.json' assert { type: "json" };
 
 const Query = {
 	hello: (_, { name }) => `Hello ${name || "World"}!`,
-	getTodos: () => todos
+	getTodos: () => todos,
+	getTodo: ( _, { id } ) => {
+		return todos[id - 1]
+	}
 }
 
 export default Query;
